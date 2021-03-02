49bf2c6f90e11f70a21ff89fcb7c1df73757c275
Gaurav Singh <2363826+0xf17@users.noreply.github.com>
2021-03-02 21:31:01 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <meta name="robots" content="noindex">
+        <title>aux.mathscapes.xyz</title>
+    </head>
+    <body>
+        <p>aux.mathscapes.xyz. Test OK</p>
+    </body>
+</html>
